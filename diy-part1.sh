01044124cba8768bd2b4c59d69d5e79381bdaa8c
oldgiwang <100756040+oldgiwang@users.noreply.github.com>
2025-09-04 11:40:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,8 +23,8 @@ git clone https://github.com/fw876/helloworld.git package/helloworld
 # git clone https://github.com/AdguardTeam/AdGuardHome.git package/AdGuardHome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
